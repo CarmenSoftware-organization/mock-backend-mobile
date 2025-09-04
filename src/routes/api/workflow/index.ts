5b007be6a33a4bdd53a93ef9b5391855db7e27a8
thammanoon Semapru <samutpra@gmail.com>
2025-09-04 12:14:10 +0700

    Implement JWT authentication and enhance purchase request API by adding access token validation and business unit checks. Introduce error handling for missing business units and purchase requests, improving response clarity and security.

--- src/routes/api/workflow/index.ts
+++ src/routes/api/workflow/index.ts
@@ -1,8 +1,48 @@
 import type { Elysia } from "elysia";
 import { resNotImplemented } from "@/libs/res.error";
+import jwt from "@elysiajs/jwt";
+import { CheckHeaderHasAccessToken, CheckHeaderHasAppId } from "@/libs/header";
+import { tbBusinessUnit, tbPurchaseRequest } from "@/mockdata";
+import { resNotFound } from "@/libs/res.error";
 
 export default (app: Elysia) =>
   app
+
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.JWT_SECRET || "secret",
+    })
+  )
+
+  .get("/api/:bu_code/workflow/purchase-request/:pr_id", async (ctx) => {
+    const { bu_code, pr_id } = ctx.params;
+
+    const { error: errorAppId } = CheckHeaderHasAppId(ctx.headers);
+    if (errorAppId) {
+      return errorAppId;
+    }
+
+    const { error: errorAccessToken } = await CheckHeaderHasAccessToken(ctx.headers, ctx.jwt);
+    if (errorAccessToken) {
+      return errorAccessToken;
+    }
+
+    const bu = tbBusinessUnit.getBusinessUnitByCode(bu_code);
+    if (!bu) {
+      return resNotFound("Business unit not found");
+    }
+
+    const pr = tbPurchaseRequest.getPurchaseRequestById(pr_id);
+    if (!pr) {
+      return resNotFound("Purchase request not found");
+    }
+
+    return {
+      stage_role : "approve"
+    };
+  })
+  
   .get("/api/workflow/type/:type", ({ params, query, body, headers }) => {
     return Response.json(resNotImplemented, { status: 501 });
   });
